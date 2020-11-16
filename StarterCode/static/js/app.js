3acefed8891fc992961f06188aa8261b0b360036
coreymcculley <corey.mcculley@gmail.com>
2020-11-16 13:46:56 -0600

    meta data has been included

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,168 +1,206 @@
-//initializing function for belly button data
 function init() {
 
-    d3.json("../../samples.json").then((sample) => {
-        //populate the select box with the sample names
-        console.log(sample)
-        var sampleNames = sample.names[0];
-        d3.select("#selDataset")
-            .selectAll('myOptions')
-            .data(sampleNames)
-            .enter()
-            .append('option')
-            .text(function(d){return d;})
-            .attr("value",function(d,i){return d,i;});
+    // Variable to select the location of the bar chart from index.html
+    var CHART = d3.selectAll("#bar").node();
 
-        //initialize the metadata to the first sample
-        var metaData=sample.metadata[0];
-            d3.select("#sample-metadata")
-            .append("p")
-            .text( "ID: "+metaData.id)
-            .append("p")
-            .text("Ethnicity: "+metaData.ethnicity)
-            .append("p")
-            .text("Gender: "+metaData.gender)
-            .append("p")
-            .text("Age: "+metaData.age)
-            .append("p")
-            .text("Location: "+metaData.location)
-            .append("p")
-            .text("wffeq: "+metaData.wfreq)
-            .append("p")
-            .text("bbtype: "+metaData.bbtype)
-            ;
-            //console.log(metaData);
+    // Use D3 fetch to read the JSON file into importedData as the argument.
+    d3.json("samples.json").then(importedData => {
+    var data = importedData;
 
-        //initialize the plots    
-        d3.json("../../samples.json").then((sample)=>{
-            //take the first 10 samples
-            //NOTE THE INSTRUCTIONS ON THIS SAY "TOP 10", but AskBCS assures me that means first 10.
-            //That's poor wording on the instructions either way.
+    // Get just the "testsubject ids" from the data set
+    justtestsubjects = data.names;
 
-            var otu_ids=sample.samples[0].otu_ids.slice(0,10).reverse();
-            var sample_values=sample.samples[0].sample_values.slice(0,10).reverse();
-            var otu_labels=sample.samples[0].otu_labels.slice(0,10).reverse();
+    // Get just the "samples" from the data set
+    justsamples = data.samples;
 
-            //Update OTU ID to a string and add the OTU test so it looks pretty
-            var strOtuIds=otu_ids.map(function(e){return "OTU "+e.toString()});
-            console.log(strOtuIds);
-            
-            //trace 1 is for bar chart
-            var trace1 = {
-             x: sample_values,
-             y: strOtuIds,
-             labels: otu_labels,
-             marker:{color:'#dd9666'},
-             type: "bar",
-             orientation: "h"};
+    // Get just the "metadata" from the data set
+    justmetadata = data.metadata;
 
-             var layout={
-                 title:'Top 10 OTUs for subject',
-                 xaxis:{title:'Sample values'},
-                 height:400,
-                 width: 700
-             };
+    // Pre-Populate data into the Test Subject ID select dropdown list
+    // Select the d3 input element for the dropdown
+    const subjectselect = d3.select("#selDataset");
 
-            var data=[trace1];    
-            Plotly.newPlot('bar', data,layout);
+    // Build the Test Subject ID drop down list
+    justtestsubjects.forEach(namevalue =>{
+        var option = subjectselect.append("option");
+        option.text(namevalue);
+        option.attr("value",namevalue);
+    });
 
-            //The bubble chart uses all of the data
-            var bubbleIDs=sample.samples[0].otu_ids;
-            var bubbleValues=sample.samples[0].sample_values;
-            var bubbleLabels=sample.samples[0].otu_labels;
-            //trace 2 is for the bubble chart
-            var trace2={
-            x: bubbleIDs,
-            y: bubbleValues,
-            text: bubbleLabels,
-            mode: 'markers',
-            marker: {
-                color: bubbleIDs,
-                size: bubbleValues
-                }    
-            }
-            var bubbleData = [trace2];
-  
-            var layout = {
-            title:'Sample values by OTU IDs: hover for OTU labels',
-            xaxis:{title:'OTU IDs'},
-            yaxis:{title:'Sample values'},    
-            showlegend: false,
-            height: 600,
-            width: 1000
-            };
-  
-            Plotly.newPlot('bubble', bubbleData, layout);
-        
-        });
-        
-       
-        
-        });
-    }
+    // Build arrays for the primary axes and labels for the bar plot.
+    // [0] is used because on init() the first subject ID is 0 
+    let sampvalues = justsamples[0].sample_values;
+    let otuids = justsamples[0].otu_ids;
+    let otulabels = justsamples[0].otu_labels;
+    
+    // Slice the first 10 objects for plotting - bar chart requirement
+    sampvalues = sampvalues.slice(0, 10);
+    otuids = otuids.slice(0, 10);
+    otulabels = otulabels.slice(0,10);
 
+    // Format OTU ID string
+    let otuidslist = otuids.map(otuid => 'OTU ' + otuid);
 
-  
- // This function is called when a dropdown menu item is selected
- function optionChanged() {
-   // Use D3 to select the dropdown menu
-   var dropdownMenu = d3.select("#selDataset");
-   // Assign the value of the dropdown menu option to a variable
-   var sampleID = dropdownMenu.property("value");
+    // Reverse the arrays to meet Plotly's default requirements
+    sampvalues = sampvalues.reverse();
+    otuidslist = otuidslist.reverse();
+    otulabels = otulabels.reverse();
+    
+    // Trace for the the bar chart
+    var trace = {
+    x: sampvalues,
+    y: otuidslist,
+    text: otulabels,
+    type: "bar",
+    orientation: "h"
+    };
 
+    // data
+    var chartData = [trace];
 
-     //check to make sure sampleID is the correct value (It is)
-   console.log(sampleID);
-   //pull the data and update the charts based on the selected subject
-   d3.json("../../samples.json").then((sample)=>{
+    // // Apply the group bar mode to the layout
+    var layout = {
+    title: "Top 10 OTUs found in the individual subject"
+    };
 
-    //update metadata
-    var metaData=sample.metadata[sampleID];
-    d3.select("#sample-metadata")
-    .selectAll("p")
-    .remove();
-    d3.select("#sample-metadata")
-    .append("p")
-    .text( "ID: "+metaData.id)
-    .append("p")
-    .text("Ethnicity: "+metaData.ethnicity)
-    .append("p")
-    .text("Gender: "+metaData.gender)
-    .append("p")
-    .text("Age: "+metaData.age)
-    .append("p")
-    .text("Location: "+metaData.location)
-    .append("p")
-    .text("wffeq: "+metaData.wfreq)
-    .append("p")
-    .text("bbtype: "+metaData.bbtype)
-    ;
+    // Render the plot to the div tag with id "bar"
+    Plotly.newPlot(CHART, chartData, layout);
+    
+    //***********************************
+    // Plot the Bubble chart  
+    //***********************************
+    var trace1 = {
+    x: justsamples[0].otu_ids,
+    y: justsamples[0].sample_values,
+    text: justsamples[0].otu_labels,
+    mode: 'markers',
+    marker: {
+        size: justsamples[0].sample_values,
+        color: justsamples[0].otu_ids,
+        colorscale: [[0,'rgb(0,0,255)'],[1,'rgb(255,0,0)']]
+        }
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+        title: 'All samples taken for the individual subject',
+        showlegend: false
+    };
+    Plotly.newPlot('bubble', data, layout);
 
-    //get appropriate values for subject for the bar and bubble charts
-    var otu_ids=sample.samples[sampleID].otu_ids.slice(0,10).reverse();
-    var sample_values=sample.samples[sampleID].sample_values.slice(0,10).reverse();
-    var otu_labels=sample.samples[sampleID].otu_labels.slice(0,10).reverse();
-    var upstrOtuIds=otu_ids.map(function(e){return "OTU "+e.toString()});
-    var bubbleIDs=sample.samples[sampleID].otu_ids;
-    var bubbleValues=sample.samples[sampleID].sample_values;
-    var bubbleLabels=sample.samples[sampleID].otu_labels;
-    marker={
-        color: bubbleIDs,
-        size: bubbleValues
-        };     
-  
+    //*************************************
+    // Populate the Demographic Data panel
+    //*************************************
+    d3.select("#sample-metadata").append("p").text('ID: '+ justmetadata[0].id);
+    d3.select("#sample-metadata").append("p").text('Ethnicity: '+ justmetadata[0].ethnicity);
+    d3.select("#sample-metadata").append("p").text('Gender: '+ justmetadata[0].gender);
+    d3.select("#sample-metadata").append("p").text('Age: '+ justmetadata[0].age);
+    d3.select("#sample-metadata").append("p").text('Location: '+ justmetadata[0].location);
+    d3.select("#sample-metadata").append("p").text('Bbtype: '+ justmetadata[0].bbtype);
+    d3.select("#sample-metadata").append("p").text('Wfreq: '+ justmetadata[0].wfreq);
+    
+     //***********************************************
+     // Plot the Belly button washing Gauge chart 
+     //***********************************************
+    initGaugeChart();
+    });
+};  
+  // End of init() function
 
+  //***********************************************************
+  // Call updatePlotly() when a change takes place to the DOM
+  //***********************************************************
 
-   // update the plots
-    Plotly.restyle("bar", "x", [sample_values]);
-    Plotly.restyle("bar", "y", [upstrOtuIds]);
-    Plotly.restyle("bar","labels",[otu_labels]);
-    Plotly.restyle("bubble", "x", [bubbleIDs]);
-    Plotly.restyle("bubble", "y", [bubbleValues]);
-    Plotly.restyle("bubble","text",[bubbleLabels]);
-    Plotly.restyle("bubble","marker",[marker]);
-   })
+d3.selectAll("body").on("change", updatePlotly);
+
+var CHART = d3.selectAll("#bar").node();
+
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    var subjectselect = d3.select("#selDataset");
+
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = subjectselect.node().value;
+
+    // Find the index of the subject in the justtestsubjects array
+    // This is necessary to find all of the dependant data for the 
+    // selected subject
+    let subjectindex = justtestsubjects.indexOf(dataset);
+    
+    // Build arrays for the primary axes and labels for the bar plot.
+    let sampvalues = justsamples[subjectindex].sample_values;
+    let otuids = justsamples[subjectindex].otu_ids;
+    let otulabels = justsamples[subjectindex].otu_labels;
+
+    // Slice the first 10 objects for plotting - bar chart requirement
+    sampvalues = sampvalues.slice(0, 10);
+    otuids = otuids.slice(0, 10);
+    otulabels = otulabels.slice(0,10);
+
+    // Format OTU ID string
+    let otuidslist = otuids.map(otuid => 'OTU ' + otuid);
+
+    // Reverse the array due to Plotly's defaults
+    sampvalues = sampvalues.reverse();
+    otuidslist = otuidslist.reverse();
+    otulabels = otulabels.reverse();
+
+    // Trace variables for the the bar chart
+    x = sampvalues;
+    y = otuidslist;
+    text = otulabels;
+
+    //***********************************
+    // Re-style the Bar chart  
+    //***********************************
+    Plotly.restyle(CHART, "x", [x]);
+    Plotly.restyle(CHART, "y", [y]);
+    Plotly.restyle(CHART, "text", [text]);
+
+    //***********************************
+    // Re-Plot the Bubble chart  
+    //***********************************
+    var trace1 = {
+        x: justsamples[subjectindex].otu_ids,
+        y: justsamples[subjectindex].sample_values,
+        text: justsamples[subjectindex].otu_labels,
+        mode: 'markers',
+        marker: {
+        size: justsamples[subjectindex].sample_values,
+        color: justsamples[subjectindex].otu_ids,
+        colorscale: [[0,'rgb(0,0,255)'],[1,'rgb(255,0,0)']]
+        }
+    };
+
+    var data = [trace1];
+    
+    var layout = {
+        title: 'All samples taken for the individual subject',
+        showlegend: false
+    };
+
+    Plotly.newPlot('bubble', data, layout);
+
+    // Demographic Data Refresh after first clearing the previously selected/updated demographic data
+
+    d3.select("#sample-metadata").selectAll('p').remove();
+
+    d3.select("#sample-metadata").append("p").text('ID: '+ justmetadata[subjectindex].id);
+    d3.select("#sample-metadata").append("p").text('Ethnicity: '+ justmetadata[subjectindex].ethnicity);
+    d3.select("#sample-metadata").append("p").text('Gender: '+ justmetadata[subjectindex].gender);
+    d3.select("#sample-metadata").append("p").text('Age: '+ justmetadata[subjectindex].age);
+    d3.select("#sample-metadata").append("p").text('Location: '+ justmetadata[subjectindex].location);
+    d3.select("#sample-metadata").append("p").text('Bbtype: '+ justmetadata[subjectindex].bbtype);
+    d3.select("#sample-metadata").append("p").text('Wfreq: '+ justmetadata[subjectindex].wfreq);
+
+     //*******************************************************************
+     // Call function to restyle Belly button washing Gauge chart restyle
+     //*******************************************************************
+
+    restyleGaugeChart(subjectindex);
 }
+  // End of updatePlotly()
 
-//call the init function
 init();
